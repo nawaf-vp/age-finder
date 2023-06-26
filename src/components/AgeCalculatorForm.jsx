4abc2c750e7416ff5b86b1829ae008f53c09088c
Nawaf v p <nawafvp.dev@gmail.com>
2023-06-26 21:38:52 +0530

    latest commit

--- src/components/AgeCalculatorForm.jsx
+++ src/components/AgeCalculatorForm.jsx
@@ -18,13 +18,13 @@ const AgeCalculatorForm = ({ calculateAge }) => {
       className='flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4'
     >
       <input
-        className='bg-orange-300 border border-orange-400 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-orange-400 dark:border-orange-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+        className='bg-orange-300 border border-orange-400 text-gray-900 text-sm shadow-2xl rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-orange-400 dark:border-orange-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
         value={birthDate}
         type='date'
         onChange={handleChangeDate}
       />
       <button
-        className='disabled:bg-slate-400 disabled:cursor-not-allowed text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-950 dark:hover:bg-blue-950 focus:outline-none dark:focus:ring-blue-950'
+        className='disabled:bg-slate-400 disabled:cursor-not-allowed text-white  shadow-2xl bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-950 dark:hover:bg-blue-950 focus:outline-none dark:focus:ring-blue-950'
         disabled={!birthDate}
         type='submit'
       >
